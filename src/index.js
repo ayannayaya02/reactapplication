48728f9b18fa3f293ebb04416b0ab904e34cf794
ayanna <ayannafofana.aaf@gmail.com>
2023-06-27 17:44:45 -0700

    structure

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Weather from "./weather";
+
+import ReactAnimatedWeather from "react-animated-weather";
+
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>Display Weather</h1>
+      <h2>78 °F </h2>
+
+      <ReactAnimatedWeather
+        icon={"PARTLY_CLOUDY_DAY"}
+        color={"black"}
+        size={50}
+        animate={true}
+      />
+      <Weather />
+      <br />
+      <div class="container">
+        <div>Mon</div>
+        <ReactAnimatedWeather
+          icon={"PARTLY_CLOUDY_DAY"}
+          color={"goldenrod"}
+          size={50}
+          animate={true}
+        />
+
+        <div>Tues</div>
+
+        <ReactAnimatedWeather
+          icon={"PARTLY_CLOUDY_DAY"}
+          color={"goldenrod"}
+          size={50}
+          animate={true}
+        />
+        <div>Wed</div>
+
+        <ReactAnimatedWeather
+          icon={"PARTLY_CLOUDY_DAY"}
+          color={"goldenrod"}
+          size={50}
+          animate={true}
+        />
+        <div>Thurs</div>
+
+        <ReactAnimatedWeather
+          icon={"PARTLY_CLOUDY_DAY"}
+          color={"goldenrod"}
+          size={50}
+          animate={true}
+        />
+        <div>Fri</div>
+
+        <ReactAnimatedWeather
+          icon={"PARTLY_CLOUDY_DAY"}
+          color={"goldenrod"}
+          size={50}
+          animate={true}
+        />
+      </div>
+    </div>
+  </StrictMode>
+);
---
+++ src/weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function searchEngine(props) {
+  function showTemperature(event) {
+    event.preventDefault();
+    let temperature = (props.temperature * 9) / 5 + 32;
+  }
+  function showCity(event) {
+    event.preventDefault();
+  }
+  return (
+    <form onSubmit={showTemperature}>
+      <input type="search" onChange={showCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
